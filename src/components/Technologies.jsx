91bd6e4fa81ae6d493d09d1cb3eaedc723c375d5
James <jamessheldona@gmail.com>
2024-12-31 22:12:57 -0500

    first commit

---
+++ src/components/Technologies.jsx
@@ -0,0 +1,78 @@
+import { RiReactjsLine } from "react-icons/ri"
+import { IoLogoJavascript } from "react-icons/io5";
+import { FaNodeJs } from "react-icons/fa";
+import { SiMongodb } from "react-icons/si";
+import PythonIcon from "../assets/python.svg";
+import JavaIcon from "../assets/java.svg";
+import { motion } from "framer-motion";
+
+const iconVariants = (duration) => ({
+    initial: { y: -10 },
+    animate: { 
+        y: [10, -10],
+        transition: { duration: duration, ease: "linear", repeat: Infinity, repeatType: "reverse" },
+    },
+});
+
+function Technologies() {
+  return (
+    <div className="border-b border-neutral-800 pb-24">
+        <motion.h2 
+            whileInView={{ y: 0, opacity: 1}} 
+            initial={{ y: -100, opacity: 0}}
+            transition={{ duration: 0.5 }}
+            className="my-20 text-center text-4xl">Technologies
+        </motion.h2>
+        <motion.div 
+            whileInView={{ x: 0, opacity: 1}} 
+            initial={{ x: -100, opacity: 0}}
+            transition={{ duration: 0.5 }}
+            className="flex flex-wrap items-center justify-center gap-4">
+            <motion.div 
+                variants={iconVariants(1)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-3">
+                <img src={PythonIcon} className="h-20 w-20" />
+            </motion.div>
+            <motion.div 
+                variants={iconVariants(1.1)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-3">
+                <img src={JavaIcon} className="h-20 w-20" />
+            </motion.div>
+            <motion.div 
+                variants={iconVariants(1.2)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
+                <IoLogoJavascript className="text-7xl text-yellow-300 "/>
+            </motion.div>
+            <motion.div 
+                variants={iconVariants(1.3)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
+                <FaNodeJs className="text-7xl text-green-600"/>
+            </motion.div>
+            <motion.div 
+                variants={iconVariants(1.4)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
+                <RiReactjsLine className="text-7xl text-cyan-400 "/>
+            </motion.div>
+            <motion.div 
+                variants={iconVariants(1.5)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
+                <SiMongodb className="text-7xl text-green-500"/>
+            </motion.div>
+        </motion.div >
+    </div>
+  )
+}
+
+export default Technologies
---
+++ src/constants/index.js
@@ -0,0 +1,57 @@
+import rice from "../assets/projects/rice.ico";
+import SafeAndSound from "../assets/projects/SafeandSound.png";
+import FormCheckAI from "../assets/projects/FCAI.png";
+import calculator from "../assets/projects/calculator.png";
+
+export const HERO_CONTENT = `I am a second-year Software Engineering student at Concordia University with practical experience in software/agile development and strong foundation in teamwork, problem-solving, and communication. I have developed various technical projects, including a Peer Assessment Application, a noise monitoring program ("Safe and Sound"), and a fitness application (FormCheck AI), showcasing my expertise in programming languages such as Python, Java, and JavaScript, along with tools like OpenCV, Node.js, and React.`;
+
+export const ABOUT_TEXT = `I am a second-year Software Engineering student at Concordia University, driven by a passion for problem-solving and creating impactful software solutions. With a strong foundation in programming and hands-on experience in full-stack development, I have contributed to projects ranging from a Peer Assessment Application to fitness and noise monitoring applications. My technical skills include proficiency in Python, Java, JavaScript, and frameworks like React and Node.js. 
+            Outside of academics, I bring adaptability and attention to detail from my experience as a Contractor Apprentice, where I worked on renovations requiring precision and creativity. I thrive in collaborative environments, as demonstrated by my participation in hackathons and teamwork-driven software projects. Fluent in English and French, with a working knowledge of Tagalog, I enjoy music, gaming, cooking, and fitness. 
+            I'm always eager to tackle new challenges and expand my technical and personal horizons.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2018 - Present",
+    role: "Contractor Apprentice",
+    company: "Jimmy Bautista, Jr.",
+    description: `Renovated kitchens, bathrooms, and other interior spaces, developing skills in plastering, painting, and flooring.`,
+    technologies: ["Adaptability", "Attention to detail", "High client satisfaction"],
+  }
+];
+
+export const PROJECTS = [
+  {
+    title: "Peer Assessment Application (Full-Stack)",
+    image: rice,
+    description:
+      "Developed a peer assessment application that facilitates student evaluations of team contributions based on dimensions such as cooperation, conceptual contribution, practical contribution, and work ethic. This project follows the Agile development approach, utilizing GitHub for version control and task management over a 10-week period, delivering four incremental prototypes.",
+    technologies: ["NodeJS", "React", "MongoDB"],
+  },
+  {
+    title: "Safe and Sound (Back-End)",
+    image: SafeAndSound,
+    description:
+      "Developed a noise monitoring program using Python, designed to help cities track noise pollution patterns. Created at PolyHacks 2024, the project involved using Matplotlib for visualizations and expanded our understanding of sound analysis.",
+    technologies: ["Python"],
+  },
+  {
+    title: "FormCheck AI ",
+    image: FormCheckAI,
+    description:
+      "Built a fitness application to help users check their exercise form and count repetitions for ConUHacks VIII. Designed for beginner gym-goers, the program analyzes body movements through a camera, providing feedback on rep quality.",
+    technologies: ["Python", "OpenCV", "MediaPipe", "HTML", "CSS", "JavaScript (Front-End)"],
+  },
+  {
+    title: "Arithmetic Expression Calculator",
+    image: calculator,
+    description:
+      "Designed and implemented a custom arithmetic calculator in Java, handling complex expressions with twoexpandable array-based stacks. The project required building the stack data structure from scratch, achieving O(1) amortized time complexity and O(n) space complexity, supporting various operators and outputting results to a file.",
+    technologies: ["Java"],
+  },
+];
+
+export const CONTACT = {
+  address: "13389 Bois-des-Trottier, QC, Montreal ",
+  phoneNo: "+438 878 3515 ",
+  email: "jamessheldona@gmail.com",
+};
